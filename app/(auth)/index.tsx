53345204f9fd8ee35ea9da79cff7899f5c87be25
Karanpreet Singh <karanx7230@gmail.com>
2026-06-19 13:43:38 +0530

    refactor: simplify Index component and remove unused code

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,29 +1,24 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
+import { StyleSheet, Text,} from "react-native";
 import { useRouter } from "expo-router";
-import { LinearGradient} from "expo-linear-gradient";
-export default function Index({ navigation }: any) { 
+import { LinearGradient } from "expo-linear-gradient";
+export default function Index() {
   const router = useRouter();
   useEffect(() => {
     const timer = setTimeout(() => {
       router.replace("/welcome");
-      // router.replace("/(tabs)");
     }, 2000);
     return () => clearTimeout(timer);
   }, []);
   return (
-    <View>
-      <LinearGradient colors={["#9675fa71", "#9775FA"]} style={style.gradient}>
-        <Text style={style.text}>Laza..</Text>
-      </LinearGradient>
-    </View>
+    <LinearGradient colors={["#9675fa71", "#9775FA"]} style={style.gradient}>
+      <Text style={style.text}>Laza</Text>
+    </LinearGradient>
   );
 }
 const style = StyleSheet.create({
-  view: {
-    flex: 1,
-  },
   gradient: {
+    flex: 1,
     height: "100%",
     width: "100%",
     justifyContent: "center",
